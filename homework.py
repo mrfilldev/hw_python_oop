d3d362100ee69cd622256dff2d4af65d79aa6e7f
mrfilldev <mr.filldev@gmail.com>
2021-08-28 12:27:02 +0300

    commit

--- homework.py
+++ homework.py
@@ -73,7 +73,27 @@ class CashCalculator(Calculator):
             return "Денег нет, держись"
         elif local_limit > day_result:
             return f"На сегодня осталось \
-                {round(local_limit - day_result, 2)} {currency}"
+{round(local_limit - day_result, 2)} {currency}"
         elif local_limit < day_result:
             return f"Денег нет, держись: твой долг - \
-                {round(abs(local_limit-day_result), 2)} {currency}"
+{round(abs(local_limit-day_result), 2)} {currency}"
+
+
+
+
+
+# создадим калькулятор денег с дневным лимитом 1000
+cash_calculator = CashCalculator(1000)
+
+# дата в параметрах не указана,
+# так что по умолчанию к записи
+# должна автоматически добавиться сегодняшняя дата
+cash_calculator.add_record(Record(amount=145, comment='кофе'))
+# и к этой записи тоже дата должна добавиться автоматически
+cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+# а тут пользователь указал дату, сохраняем её
+cash_calculator.add_record(Record(amount=3000,
+                                  comment='бар в Танин др',
+                                  date='08.11.2019'))
+
+print(cash_calculator.get_today_cash_remained('rub'))
