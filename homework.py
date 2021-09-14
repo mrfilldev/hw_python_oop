2cec34f432c3afe849e0af213353c51cf43c58f8
mrfilldev <mr.filldev@gmail.com>
2021-09-14 15:30:31 +0300

    ревью 6

--- homework.py
+++ homework.py
@@ -71,31 +71,14 @@ class CashCalculator(Calculator):
         "rub": ["руб", RUB_RATE],
     }
 
-    def currency_is(self, currency):
-        local_limit = round(self.limit / self.money_dict[currency][1], 2)
-        day_result = round(
-            self.get_today_stats() / self.money_dict[currency][1],
-            2)
-        currency = self.money_dict[currency][0]
-        result = (currency, local_limit, day_result)
-        return result
-
     def get_today_cash_remained(self, currency) -> str:
-        check_zero = round(self.result_limit_get_today_status(), 2)
-        if check_zero == 0:
+        today_cash = round(self.result_limit_get_today_status(), 2)
+        if today_cash == 0:
             return "Денег нет, держись"
-        if currency == "usd":
-            result = self.currency_is(currency)
-        if currency == "eur":
-            result = self.currency_is(currency)
-        if currency == "rub":
-            result = self.currency_is(currency)
-
-        if result[1] > result[2]:
-            remains = round(result[1] - result[2], 2)
+        today_cash = round(today_cash / self.money_dict[currency][1], 2)
+        if today_cash > 0:
             return (f"На сегодня осталось "
-                    f"{remains} {result[0]}")
-        if result[1] < result[2]:
-            debt = round(result[2] - result[1], 2)
+                    f"{today_cash} {self.money_dict[currency][0]}")
+        else:
             return (f"Денег нет, держись: твой долг - "
-                    f"{debt} {result[0]}")
+                    f"{abs(today_cash)} {self.money_dict[currency][0]}")
